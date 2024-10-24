72b435ac79d2bad51f930a5130455d4ae73847bd
shifamahveen <mahveenshifa1611@gmail.com>
2024-10-24 12:04:17 +0530

    Form validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import './App.css';
-import Timer from "./components/Timer";
+import Form from "./components/Form";
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showTimer: true
-    }
-  };
-
-  removeTimer = () => {
-    this.setState({ showTimer: false });
-  };
 
   render() {
     return (
       <>
-        {this.state.showTimer ? <Timer /> : <p> Timer Component Unmounted </p> }
-        <br />
-        <button onClick={this.removeTimer}>Remove Timer Component</button>
+       <Form />
       </>
     )
   }
